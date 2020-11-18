747f14b04930e624c0fae26cc3486c1b91327e42
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2020-11-17 23:22:42 -0600

    hex to base64

--- basic/hex_to_base_64.py
+++ basic/hex_to_base_64.py
@@ -1,3 +1,12 @@
-# Creating
+# Converting from Hexadecimal string to Base64
+
+import base64
+
+input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
 
+input_hex = bytes.fromhex(input)
+o = base64.encodebytes(input_hex)
+print(o)
