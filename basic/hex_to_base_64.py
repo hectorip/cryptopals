d0db9438e6613b867b10f50d9dbb97b7c5512a1c
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2020-11-18 23:59:37 -0600

    WIP

--- basic/hex_to_base_64.py
+++ basic/hex_to_base_64.py
@@ -1,8 +1,10 @@
-# Converting from Hexadecimal string to Base64
+# Convert from Hexadecimal string to Base64
 
 import base64
 
 input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+# Should this be converted to bytes?
 
 output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
